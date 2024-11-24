154b931a4c5cb8838475ec81687c4ed288b6833e
hibikeita18 <hibikeita18@gmail.com>
2024-11-24 16:55:56 +0000

    add(update): project-idx

---
+++ .idx/dev.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }: {
+  channel = "stable-24.05";
+
+  packages = [
+    pkgs.mise
+  ];
+
+  env = {};
+  idx = {
+    extensions = [
+      "ms-python.python"
+      "ms-python.debugpy"
+      "ms-python.mypy-type-checker"
+      "charliermarsh.ruff"
+    ];
+
+    workspace = {
+      onCreate = {
+        mise-install = "mise install";
+        pdm-install = "pdm install";
+      };
+      onStart = {
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
